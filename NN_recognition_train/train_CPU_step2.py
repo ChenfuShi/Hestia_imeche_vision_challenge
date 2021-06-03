ee384302e457e34834cda35223287de1d2237fe1
chenfu.shi@postgrad.manchester.ac.uk <https://chenfushi@github.com>
2021-06-03 22:32:45 +0100

    cleanup

---
+++ NN_recognition_train/train_CPU_step2.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+import numpy as np
+import random
+import tensorflow as tf
+import tensorflow.keras as k
+import glob
+from PIL import Image
+import argparse
+import sys
+tf.config.threading.set_intra_op_parallelism_threads(16)
+tf.config.threading.set_inter_op_parallelism_threads(16)
+from model.step_2_GAP import train_this
+
+
+
+
+if __name__ == '__main__':
+
+    model_name = sys.argv[1]
+
+    print(f"MODEL NAME = {model_name}")
+
+    train_this(model_name)
