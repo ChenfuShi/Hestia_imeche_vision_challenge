cc09f93f8cb7cb96c945f5cd2717968720c68ce8
chenfushi <chenfu.shi@postgrad.manchester.ac.uk>
2021-06-04 11:04:04 +0100

    basic functionality

--- square_recognition/camera_nn_part.py
+++ square_recognition/camera_nn_part.py
@@ -1,4 +1,3 @@
-from typing_extensions import ParamSpecArgs
 import cv2
 import signal
 import sys
@@ -8,6 +7,8 @@ from picamera import PiCamera
 import datetime
 import time
 import numpy as np
+import threading
+import pandas as pd
 
 from camera_class import camera_obj
 
@@ -17,22 +18,52 @@ class NN_recognition():
 
     def initialize_script(self):   
         self.state = "on"     
-        camera = camera_obj()
+        self.camera = camera_obj()
+
+        # dronekit stuff
+
+
+        # results
+        self.results = []
+
+        thread = threading.Thread(target=self.run_loop, args=())
+        thread.daemon = True                            # Daemonize thread (allow main to die)
+        thread.start()                                  # Start the execution
+
+    def end_script(self):
+        self.state = "off"
+        time.sleep(0.5) # wait for run_loop to terminate
+
+        self.camera.shutdown()
+        # run finalization and send results back
+
+    def run_loop(self):
         while(True):
             # Capture frame-by-frame
-            frame = camera.retrieve_frame()
+            frame = self.camera.retrieve_frame()
+
+            # retrieve dronekit telemetry
+
+            # run neural network 1
+
+
+            # if result from neural network 1 is good then run neural network 2
+
+
+
+            # save results
 
             # Display the resulting frame
             cv2.imshow('frame',frame)
             if (cv2.waitKey(1) & 0xFF == ord('q')) or self.state == "off":
+                self.state = "off"
                 break
-        camera.shutdown()
+        
         cv2.destroyAllWindows()
+        
 
-    def end_script(self):
-        self.state = "off"
-
-
+    def finalize_results(self):
+        pass
 
 
 
